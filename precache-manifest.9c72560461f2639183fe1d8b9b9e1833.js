0b31f06d947772ebe9785e4f370ec4c401df4f8e
Daniel Noe <dnoe1010@gmail.com>
2020-04-21 01:19:00 -0700

    Updates

---
+++ precache-manifest.9c72560461f2639183fe1d8b9b9e1833.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e54f46230608a700ad40788047d513",
+    "url": "/forex-clock/index.html"
+  },
+  {
+    "revision": "14976265a3dc38b711e9",
+    "url": "/forex-clock/static/css/main.62921451.chunk.css"
+  },
+  {
+    "revision": "eaf262e9e30668cbcb27",
+    "url": "/forex-clock/static/js/2.e2b0c5c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forex-clock/static/js/2.e2b0c5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14976265a3dc38b711e9",
+    "url": "/forex-clock/static/js/main.7d62a231.chunk.js"
+  },
+  {
+    "revision": "2d85e59381136f95e4a5",
+    "url": "/forex-clock/static/js/runtime-main.fa025560.js"
+  },
+  {
+    "revision": "d4e6dda72ad732dc3013ec9f3903ec92",
+    "url": "/forex-clock/static/media/moon.d4e6dda7.svg"
+  },
+  {
+    "revision": "361e278b397254bc9a8db59b9cc0aac9",
+    "url": "/forex-clock/static/media/sun.361e278b.svg"
+  }
+]);
